81228e6a49a16c31afd11ad429f55136dcb5df2d
Cyrus Tafti <cyrustafti@gmail.com>
2026-07-04 15:22:48 -0400

    stop tracking vendored Ableton Link tree

--- sidecar-experiment/link/AbletonLinkConfig.cmake
+++
@@ -1,7 +0,0 @@
-cmake_minimum_required(VERSION 3.10)
-
-set(CMAKE_CXX_STANDARD 17)
-
-include(${CMAKE_CURRENT_LIST_DIR}/cmake_include/ConfigureAbletonLink.cmake)
-ConfigureAbletonLink(${CMAKE_CURRENT_LIST_DIR})
-
